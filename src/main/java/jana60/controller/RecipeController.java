96ee0422ab80d4f3242004b33f4ce170b6567cd4
EspoTony <espotony98@gmail.com>
2022-08-31 17:45:09 +0200

    added recipes controller list

--- src/main/java/jana60/controller/RecipeController.java
+++ src/main/java/jana60/controller/RecipeController.java
@@ -38,14 +38,37 @@ public class RecipeController {
 	
 	@GetMapping("/add")
 	public String SubForm(Model model) {
-	 model.addAttribute("NuovoOggetto",new Recipe());
-		return "/add";
+	 model.addAttribute("NewRecipes",new Recipe());
+		return "recipesList";
 
  }
 	
+	/*@PostMapping("/recipeslist")
+	public String RecipeSave(@Valid @ModelAttribute("recipe") Recipe recipeadd, BindingResult br) {
+		boolean brError = br.hasErrors();
+		boolean checkName = true;
+		if (recipeadd.getId() != null) {
+			Recipe vecia = recipeRepo.findById(recipeadd.getId()).get();
+			if (vecia.getTitle().equals(recipeadd.getTitle())) {
+				checkName = false;
+			}
+		}
+		if (checkName && recipeRepo.countByTitleAllIgnoreCase(recipeadd.getTitle()) > 0) {
+			br.addError(new FieldError("recipe", "title", "Il nome deve essere unico"));
+			brError = true;
+		}
+
+		if (brError) {
+			return "returnList";
+		} else {
+			recipeRepo.save(recipeadd);
+			return "redirect:/";
+		}
+	}*/
+	
 	
 	@PostMapping("/save")
-	  public String save(@Valid @ModelAttribute("NuovoOggetto") Recipe formSub,BindingResult br) {
+	  public String save(@Valid @ModelAttribute("NewRecipes") Recipe formSub,BindingResult br) {
 	   
 		 if(br.hasErrors()) {
 			 return "/add";
